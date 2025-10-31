3090340e4353f74406a1db546a923b507a2649f1
Elbaf <georgiyelbaf@gmail.com>
2025-10-31 19:15:13 +0300

    Initial commit

---
+++ src/main/java/spring/telegrambot/drinkWaterMeter/DrinkWaterMeterApplication.java
@@ -0,0 +1,13 @@
+package spring.telegrambot.drinkWaterMeter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrinkWaterMeterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrinkWaterMeterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spring/telegrambot/drinkWaterMeter/DrinkWaterMeterApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.telegrambot.drinkWaterMeter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrinkWaterMeterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
