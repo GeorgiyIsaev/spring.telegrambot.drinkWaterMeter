37c0e59f6daf3eabc9862bac76569e067a9f849c
Elbaf <georgiyelbaf@gmail.com>
2025-11-11 06:27:29 +0300

    delete commit

--- src/main/java/spring/telegrambot/drinkWaterMeter/repository/DrinkDayRepository.java
+++
@@ -1,7 +0,0 @@
-//package spring.telegrambot.drinkWaterMeter.repository;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import spring.telegrambot.drinkWaterMeter.repository.model.user.DayDrinks;
-//
-//public interface DrinkDayRepository extends JpaRepository<DayDrinks, Integer> {
-//}
